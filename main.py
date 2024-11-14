a06d6b0108bc2d150272935688614996edb73805
ihou-david-wateba <ihoudavid.wateba@edu.esiee.fr>
2024-11-14 10:01:52 +0000

    les modifications apportées à ce code permettent de vérifié si une chaine de caractère donnée est un palindrome

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+"""Ce code vérifie si une chaîne de caractères est un palindrome
+"""
+import re
 #### Fonction secondaire
 
-
-def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+def ispalindrome(p:str)->bool:
+    """
+    cette fonction vérifie si une chaîne de caractères est un palindrome
+    en retournant True si p est un palindrome, False sinon.
+    p est La chaîne de caractères à vérifier.
+    """
+    accents = str.maketrans("éèêëàâäîïôöùûüç","eeeeaaaiioouuuc")
+    p=p.lower().translate(accents)
+    p=re.sub(r'[^a-z0-9]', '', p)
+    p_second=p[::-1]
+    return p_second == p
 
 #### Fonction principale
 
-
 def main():
-
+    """
+    Fonction principale pour tester la fonction ispalindrome avec plusieurs cas de test.
+    """
+    # Liste des cas de test
+    test_cases= [
+        "radar",# True
+        "kayak",# True
+        "civic",# True
+        "deified",# True
+        "hello",# False
+        "world",# False
+    ]
+    # Test de chaque chaîne de caractères
+    for i in test_cases:
+        print(i, ispalindrome(i))
     # vos appels à la fonction secondaire ici
-
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
